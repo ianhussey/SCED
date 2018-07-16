5b6171a194e3ec59b422b2ee9cdc172223d292be
ianhussey <ian.hussey@ugent.be>
2018-07-17 00:28:14 +0200

    plot reorders by time of intervention

--- R/sced_plot.r
+++ R/sced_plot.r
@@ -1,6 +1,6 @@
 #' Plot data
 #'
-#' Plot data from an AB design SCED experiment
+#' Plot data from an AB design SCED experiment, reordering the participants by the order in which they change from A to B condition.
 #' @param data Experiment data. This must contain columns named "Participant", "Timepoint" (integer), "Score" (numeric; your DV), and "Condition" (must include only "A" and "B" as a string or factor). See the included simulated_data dataset for an example using \code{View(simulated_data)}.
 #' @export
 #' @examples
@@ -16,15 +16,24 @@ sced_plot <- function(data) {
     group_by(Participant, Condition) %>%
     mutate(median_score = median(Score)) %>%
     ungroup()
+  
+  intervention_point <- data %>%
+    filter(Condition == "B") %>%
+    group_by(Participant) %>%
+    summarize(intervention_point = min(Timepoint)) %>%
+    ungroup()
+  
+  plot_data <- data_with_condition_change %>%
+    mutate(Participant = fct_reorder(Participant, intervention_point))
     
   plot <- 
-    ggplot(data_with_condition_change) +
+    ggplot(plot_data) +
     geom_smooth(aes(x = Timepoint, y = Score, group = Condition), method = "lm", alpha = 0.15, colour = "black") +  # or loess
     geom_line(aes(x = Timepoint, y = median_score, group = Condition), linetype = "dashed") +
     geom_point(aes(x = Timepoint, y = Score, group = Condition)) +  # , color = is_median_color
     geom_line(aes(x = Timepoint, y = Score, group = Condition)) +
     scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
-    geom_vline(aes(xintercept = data_with_condition_change$condition_change), linetype = "dotted", color = "black") +
+    geom_vline(aes(xintercept = condition_change), linetype = "dotted", color = "black") +
     theme_classic() +
     scale_color_manual(values = c("#000000", "#bc1414")) +  # second color for median highlights, to be added.
     theme(legend.position = "none") +
