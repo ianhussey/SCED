4e03a3cba97254a4ffe61e136e60a258d9c8df5b
ianhussey <ian.hussey@ugent.be>
2018-11-06 13:23:15 +0100

    analysis and meta analysis additions
    # analysis
    
    - added baseline and post intervention trends
    - added effect size inversion option to account for direction of expectations (i.e., improvement or disimprovement)
    
    # meta analysis
    
    - added hedges g meta analysis option
    - added unstandardized median median-difference score to the output
    - added option to exclude cases with baseline trends greater than a cutoff value

--- R/meta_analysis.R
+++ R/meta_analysis.R
@@ -1,11 +1,16 @@
-#' Meta analyse probability values (Ruscio's A)
+#' Meta analyse standardized effect sizes across single cases
 #'
-#' Meta analyse the robust effect sizes (Ruscio's A) provided by sced_analyse
+#' Meta analyse the standardized effect sizes (Ruscio's A or Hedges' g) provided by sced_analyse.
+#' Uses the metafor package under the hood. 
+#' Please note that the CIs reported in the meta analysis may differ from those reported by sced_analyse(), as the meta analysis uses the standard error for estimation rather than the empirical CIs, which can be asymmetric. When combined with the next point below, this means that the CIs in the meta analysis can be < 0 or > 1, outside the bounds of the observable values for Ruscio's A. If the meta analysed effect size intervals are < 0 or > 1 they can be reported as 0 or 1 instead.
+#' Meta analysis is done via a maximum likelihood random effects model using a gaussian link function for both Ruscio's A and Hedges' g effect sizes. In the case of Ruscio's A, this means that effect sizes are non parametric at the individual level, but the underlying effect is assumed to vary normally between participants. Other strategies for the meta analysis of probability are possible (e.g., some have employed logit link functions), but due to its relative rarity no best practices have yet emerged. 
 #' @param results Output of sced_analysis().
+#' @param effect_size Which standardized effect size should be meta analysed. Must be set to either 'ruscios_A', 'A', 'hedges_g', or 'g'. Ruscio's A is more robust; Hedges' g is included for familiarity.
+#' @param baseline_trend_exclusion_criterion If set to a numeric value, cases with a baseline trend (ie standardized beta OLS regression value for the timepoint A scores) with an absolute value greater than this numeric value will be excluded from the meta analysis.
 #' @export
 #' @examples
 #' # meta analysis
-#' meta_analysis <- sced_meta_analysis(results = results)
+#' meta_analysis <- sced_meta_analysis(results = results, effect_size = "ruscios_A")
 #' 
 #' # return results
 #' meta_analysis$meta_effect
@@ -22,25 +27,56 @@
 #' text( 8.4, 6.8, "Ruscio's A [95% CI]", pos = 2)
 #' 
 
-sced_meta_analysis <- function(results) {
+sced_meta_analysis <- function(results, effect_size, baseline_trend_exclusion_criterion = NULL) {
   require(tidyverse)
   require(metafor)
   require(timesavers)
   
+  # exclude participants due to baseline trends
+  if (is.numeric(baseline_trend_exclusion_criterion)) {
+    
+    results <- results %>%
+      filter(abs(`Baseline trend`) <= baseline_trend_exclusion_criterion)
+    
+  }
+  
   # get data
+  if (effect_size %in% c("ruscios_A", "A")) {
+    
+    yi <- results$ruscios_A
+    vi <- results$ruscios_A_se^2
+    
+    es_label <- "Meta analysed Ruscio's A"
+    es_label_2 <- ", Ruscio's A = "
+    
+  } else if (effect_size %in% c("hedges_g", "g")) {
+    
+    yi <- results$hedges_g
+    vi <- results$hedges_g_se^2
+    
+    es_label <- "Meta analysed Hedges' g"
+    es_label_2 <- ", Hedges' g = "
+    
+  } else {
+    print("effect_size must be set to 'ruscios_A', 'A', 'hedges_g', or 'g'")
+  }
+  
   data_for_meta_analysis <- results %>%
-    dplyr::mutate(
-      yi = ruscios_A,
-      # convert SE to variance
-      vi = ruscios_A_se^2
-    )
+    dplyr::mutate(yi = yi,
+                  vi = vi)  # convert SE to variance
+  
+  # robust unstandardized effect size: median (median difference between conditions)
+  
+  mdn_mdn_diff <- results %>%
+    summarize(mdn_mdn_diff = median(median_difference)) %>%
+    pull(mdn_mdn_diff)
   
   # fit Random Effects model using metafor package
   meta_fit <- rma(yi, vi,
                   data = data_for_meta_analysis,
                   slab = paste(Participant))
   
-  # make predictions converting to odds ratios
+  # make predictions
   predictions <-
     predict(meta_fit) %>%
     as.data.frame() %>%
@@ -49,17 +85,19 @@ sced_meta_analysis <- function(results) {
     dplyr::rename(metric = key,
                   estimate = value) %>%
     mutate(metric = dplyr::recode(metric,
-                                  "pred" = "Meta analysed Ruscio's A",
+                                  "pred" = es_label,
                                   "ci.lb" = "95% CI lower",
                                   "ci.ub" = "95% CI upper",
                                   "cr.lb" = "95% CR lower",
                                   "cr.ub" = "95% CR upper"))
   
+  # create results strings
   meta_effect_string <- 
-    paste0("Meta analysis: k = ", meta_fit$k, 
-           ", Ruscio's A = ", predictions$estimate[1],
-           ", 95% CI [", predictions$estimate[3], ", ", predictions$estimate[4], "]",
-           ", 95% CR [", predictions$estimate[5], ", ", predictions$estimate[6], "]")
+    paste0("Meta analysis: k = ", meta_fit$k,  # number of single cases
+           "median (median difference between conditions) across cases = ", mdn_mdn_diff,  # median (median difference between conditions) across participants
+           es_label_2, predictions$estimate[1],  # standardized ES
+           ", 95% CI [", predictions$estimate[3], ", ", predictions$estimate[4], "]",  # 95% Confidence interval (long run contains mean standardized ES)
+           ", 95% CR [", predictions$estimate[5], ", ", predictions$estimate[6], "]")  # 95% Credibility interval (long run contains observed standardized ES)
   
   meta_heterogeneity_string <- 
     paste0("Heterogeneity tests: Q(df = ", meta_fit$k - 1, ") = ", round(meta_fit$QE, 2), 
@@ -68,9 +106,13 @@ sced_meta_analysis <- function(results) {
            ", I^2 = ",   round(meta_fit$I2, 2),
            ", H^2 = ",   round(meta_fit$H2, 2))
   
-  # print results
-  return(list(model_fit = meta_fit,
-              meta_analysed_effect_size = predictions,
+  # returns list of results
+  return(list(median_median_difference_unstandardized_effect_size = mdn_mdn_diff,
+              model_fit = meta_fit,
+              meta_analysed_standardized_effect_size = predictions,
               meta_effect = meta_effect_string,
               meta_heterogeneity = meta_heterogeneity_string))
+  
 }
+
+
--- R/sced_analysis.r
+++ R/sced_analysis.r
@@ -2,6 +2,10 @@
 #'
 #' Analyse data from an AB design SCED experiment using non-parametric frequentist tests
 #' @param data Experiment data. This must contain columns named "Participant", "Timepoint" (integer), "Score" (numeric; your DV), and "Condition" (must include only "A" and "B" as a string or factor). See the included simulated_data dataset for an example using \code{View(simulated_data)}.
+#' @param n_boots: number of bootstrapped resamples for Hedges' g and Ruscio's A. N for p value permutation is n_boots*10. 
+#' @param invert_effect_sizes: Effect sizes are reported assuming that scores in timepoint B are expected to be higher than timepoint A (i.e., that the intervention causes scores to increase). If invert_effect_sizes == TRUE then effect sizes are inverted, e.g., if the intervention is expected to causes scores to decrease.
+#' @return Baseline trend: standardized beta OLS regression coefficient for the slope between the timepoint A data points. Treats the timepoints as equally spaced integers (e.g., rather than modelling them as dates). Can be used to exclude participants from consideration in meta analysis, e.g., on the basis that improvements at followup are due to improvement trends at baseline.
+#' @return Intervention trend: standardized beta OLS regression coefficient for the slope between the timepoint B data points. Treats the timepoints as equally spaced integers (e.g., rather than modelling them as dates). 
 #' @return p: Hypothesis test p value via permutation test. Calculated via Monte-Carlo simulation (10000 runs) rather than brute force.
 #' @return median_difference: Difference between the median value of A and the median value of B. Because Ruscio's A is non-parametric it can notionally suffer from ceiling effects, e.g., where A = 1.00 but the real difference between the groups are larger or smaller. It can therefore be useful to report the unstandardized effect size as well as the standardized effect sizes. Median difference is used here over mean difference given its greater robustness.
 #' @return ruscios_A: Effect size A (Ruscio, 2008). The probability that a randomly selected timepoint in condition B is larger than a randomly selected timepoint in condition A. Ranges from 0 to 1, where 0.5 is equal chance. Highly similar to Area Under the Curve (AUC)/the Common Language Effect Size (CLES)/the probability of superiority but with no parametric assumptions. A Cohen's d of 1.5 corrisponds to an A of 0.85.
@@ -10,12 +14,45 @@
 #' @examples
 #' sced_results <- sced_analysis(data = simulated_data)
 
-sced_analysis <- function(data, n_boots = 2000) {
+sced_analysis <- function(data, n_boots = 2000, invert_effect_sizes = FALSE) {
   require(tidyverse)
   require(coin)
   require(effsize)
   require(bootES)
   data(simulated_data)
+  
+  # trends at baseline and post intervention
+  baseline_trend <- data %>%
+    filter(Condition == "A") %>%
+    # standardize data
+    mutate(standardized_score = as.numeric(scale(Score))) %>%
+    # fit linear model for each participant
+    group_by(Participant) %>%
+    mutate(timepoint_integer          = row_number(),
+           timepoint_integer_centered = as.numeric(scale(timepoint_integer))) %>%
+    do(tidy(lm(standardized_score ~ timepoint_integer_centered, data = .))) %>%
+    ungroup() %>%
+    # extract standardized beta estimates
+    filter(term == "timepoint_integer_centered") %>%
+    # tidy names
+    mutate(`Baseline trend`    = round(estimate, 2)) %>%
+    select(Participant, `Baseline trend`)
+  
+  post_intervention_trend <- data %>%
+    filter(Condition == "B") %>%
+    # standardize data
+    mutate(standardized_score = as.numeric(scale(Score))) %>%
+    # fit linear model for each participant
+    group_by(Participant) %>%
+    mutate(timepoint_integer = row_number(),
+           timepoint_integer_centered = as.numeric(scale(timepoint_integer))) %>%
+    do(tidy(lm(standardized_score ~ timepoint_integer_centered, data = .))) %>%
+    ungroup() %>%
+    # extract standardized beta estimates
+    filter(term == "timepoint_integer_centered") %>%
+    # tidy names
+    mutate(`Intervention trend` = round(estimate, 2)) %>%
+    select(Participant, `Intervention trend`)
   
   # p values via non-parametric permutation tests
   p_by_participant <- data %>%
@@ -29,10 +66,10 @@ sced_analysis <- function(data, n_boots = 2000) {
   
   median_change <- data %>%
     group_by(Participant) %>%
-    dplyr::summarize(median_a = median(Score[Condition == "A"]),
-                     median_b = median(Score[Condition == "B"]),
+    dplyr::summarize(median_a          = median(Score[Condition == "A"]),
+                     median_b          = median(Score[Condition == "B"]),
                      median_difference = median_b - median_a,
-                     na.rm = TRUE) %>%
+                     na.rm             = TRUE) %>%
     dplyr::select(Participant, median_difference)
   
   # bootstrapped Ruscio's nonparametric effect size A
@@ -54,13 +91,13 @@ sced_analysis <- function(data, n_boots = 2000) {
     
     fit <- data %>%
       bootES(.,
-             R = n_boots,
-             data.col = "Score",
-             group.col = "Condition",
-             contrast = c(A = -1, B = 1),
+             R           = n_boots,
+             data.col    = "Score",
+             group.col   = "Condition",
+             contrast    = c(A = -1, B = 1),
              effect.type = "hedges.g",
-             ci.type = "bca",
-             ci.conf = 0.95)
+             ci.type     = "bca",
+             ci.conf     = 0.95)
     
     results <- data.frame(hedges_g        = round(fit$t0,        3),
                           hedges_g_se     = round(sd(fit$t),     3),
@@ -74,10 +111,23 @@ sced_analysis <- function(data, n_boots = 2000) {
     ungroup()
   
   # combine results
-  results <- p_by_participant %>%
-    left_join(median_change, by = "Participant") %>%
+  results <- baseline_trend %>%
+    left_join(post_intervention_trend,       by = "Participant") %>%
+    left_join(p_by_participant,              by = "Participant") %>%
+    left_join(median_change,                 by = "Participant") %>%
     left_join(ruscios_A_boot_by_participant, by = "Participant") %>%
-    left_join(hedges_g_by_participant, by = "Participant")
+    left_join(hedges_g_by_participant,       by = "Participant")
+    
+  # conditionally invert effect sizes results
+  if (invert_effect_sizes == TRUE){
+    results <- results %>%
+      mutate(ruscios_A = 1 - ruscios_A,
+             ruscios_A_ci_lwr = 1 - ruscios_A_ci_lwr,
+             ruscios_A_ci_upr = 1 - ruscios_A_ci_upr,
+             hedges_g = hedges_g*-1,
+             hedges_g_ci_lwr = hedges_g_ci_lwr*-1,
+             hedges_g_ci_upr = hedges_g_ci_upr*-1)
+  }  
   
   return(results)
 }
