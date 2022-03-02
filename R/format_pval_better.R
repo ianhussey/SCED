93fb38ac38b2dcdf54a0f48c06da9a56b91c5b8f
ianhussey <ian.hussey@ugent.be>
2022-03-02 19:17:20 +0100

    Update format_pval_better.R
    covered case for p > .9999

--- R/format_pval_better.R
+++ R/format_pval_better.R
@@ -10,8 +10,9 @@
 #' format_pval_better(0.7)
 
 format_pval_better <- function(p){
-  p_formatted <- ifelse(p >= 0.0001, paste("=", round(p, 4)),
-                        ifelse(p < 0.0001, "< .0001", NA))
+  p_formatted <- case_when(p > 0.9999 ~ "> 0.9999",
+                           p >= 0.0001 ~ paste("=", round(p, 4)),
+                           p < 0.0001 ~ "< .0001")
   p_formatted <- gsub(pattern = "0.", replacement = ".", x = p_formatted, fixed = TRUE)
   
   # add a trailing zero so that it is always at least 2 decimal places
